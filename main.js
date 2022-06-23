67d761979cb7cd100ae7f0935f884bed9284595d
oyemwilson <85078140+oyemwilson@users.noreply.github.com>
2022-06-23 21:05:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let myName = "Oyem Wilson Ikenna";
+let myHeight = 1.82;
+let myCountry = "Nigeria";
+document.write(
+  "My name is : " +
+    myName +
+    "<br>" +
+    " " +
+    "My height is: " +
+    myHeight +
+    "<br>" +
+    " " +
+    "My country is : " +
+    myCountry +
+    " "
+);
